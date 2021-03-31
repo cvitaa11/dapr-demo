29cdbfc91589a330ad782fb9ccb149dcf5fb4682
Ivan Cvitkovic <ivancvitkovic98@gmail.com>
2021-03-31 14:22:49 +0200

    Init commit

---
+++ dotnetConsumer/Controllers/MessagesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetConsumer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+    }
+}
---
+++ dotnetPublisher/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using Dapr;
+using Dapr.Client;
+using dotnetPublisher.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetPublisher.Controllers
+{    
+    [Route("[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+
+        private readonly DaprClient daprClient = new DaprClientBuilder().Build();
+
+        //[Topic("kafka-pubsub", "newMessage")]
+        [HttpPost]
+        public async Task<ActionResult> CreateMessage(Message msg)
+        {
+            await daprClient.PublishEventAsync<Message>("kafka-pubsub", "newMessage", msg);
+
+            return Ok(new { response = "Successfully published message." });
+        }
+    }
+}
